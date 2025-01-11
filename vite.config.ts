9d0bd5908ced38acdccc18a5f92055d18a6d642d
hashim <hashimkakkaden@gmail.com>
2025-01-11 12:48:24 +0530

    updates on vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:5173
-  }
-})
+    port: 5173,
+    proxy: {
+      "/": {
+        target: "http://localhost:8000", 
+        changeOrigin: true,
+        secure: false, 
+        rewrite: (path) => path, 
+      },
+    },
+  },
+});
