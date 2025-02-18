e763a6d7333350f2015d2918c6db9afd8148d1d3
hashim <hashimkakkaden@gmail.com>
2025-02-18 15:15:18 +0530

    communication-socket updates1.2

--- src/store/slices/BoardSlice.tsx
+++ src/store/slices/BoardSlice.tsx
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios, { AxiosError } from "axios";
+import taskApi from "../../utils/axios/taskAxios";
 import {
   createStatusColumn,
   createStatusResponse,
@@ -11,15 +11,16 @@ import {
   updateStatusResponse,
 } from "../../types/board/board";
 import { Types } from "mongoose";
+import { AxiosError } from "axios";
 
 type TaskState = {
   columns: Omit<IColumn, "team_id">[];
-  tasks:ITask[],
+  tasks: ITask[];
 };
 
 const initialState: TaskState = {
   columns: [],
-  tasks:[],
+  tasks: [],
 };
 
 export const createColumn = createAsyncThunk<
@@ -29,8 +30,8 @@ export const createColumn = createAsyncThunk<
 >("/createColumn", async (statusData, { rejectWithValue }) => {
   try {
     console.log(statusData, "statusData");
-    const response = await axios.post<createStatusResponse>(
-      "https://morrow.hashim-dev007.online/task/create-status",
+    const response = await taskApi.post<createStatusResponse>(
+      "/create-status",
       statusData
     );
     return response.data;
@@ -48,8 +49,8 @@ export const getColumn = createAsyncThunk<
   { rejectValue: string }
 >("tasks/getStatus", async ({ teamId }, { rejectWithValue }) => {
   try {
-    const response = await axios.get<getStatusResponse>(
-      `https://morrow.hashim-dev007.online/task/get-status/${teamId}`
+    const response = await taskApi.get<getStatusResponse>(
+      `/get-status/${teamId}`
     );
     return response.data;
   } catch (error) {
@@ -60,41 +61,82 @@ export const getColumn = createAsyncThunk<
   }
 });
 
-export const editColumnName = createAsyncThunk<updateStatusResponse,{name:string,id:string,team_id:string},{rejectValue:string}>('/updateColumn',async({name ,id,team_id},{rejectWithValue})=>{
+export const editColumnName = createAsyncThunk<
+  updateStatusResponse,
+  { name: string; id: string; team_id: string },
+  { rejectValue: string }
+>("/updateColumn", async ({ name, id, team_id }, { rejectWithValue }) => {
   try {
-    const response = await axios.post<updateStatusResponse>(`https://morrow.hashim-dev007.online/task/update-status/${team_id}`,{name,id});
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError<{message:string}>
-    return rejectWithValue(axiosError.response?.data.message || "failed to update the name");
-  }
-})
-
-export const deleteColumn = createAsyncThunk<deleteStatusResponse,{id:string,team_id:string},{rejectValue:string}>('/deleteColumn',async({id,team_id},{rejectWithValue})=>{
-  try {
-    const response = await axios.delete<deleteStatusResponse>(`https://morrow.hashim-dev007.online/task/delete-task/${team_id}?id=${id}`);
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError<{message:string}>
-    return rejectWithValue(axiosError.response?.data.message || "failed to delete the column");
-  }
-})
-
-export const createTask = createAsyncThunk<ITask,{name:string,id:string,status:string,priority:string,team_id:string,assignee:{_id:Types.ObjectId}[]},{rejectValue:string}>('/addTask',async({name,id,priority,status,team_id,assignee},{rejectWithValue})=>{
-  try {
-    const response = await axios.post<ITask>(`https://morrow.hashim-dev007.online/task/create-task/${team_id}?status=${status}&name=${name}&id=${id}&priority=${priority}`,{assignee});
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError<{message:string}>;
-    return rejectWithValue(axiosError.response?.data.message || "failed to create the Task")
-  }
-})
-
-export const getTasks = createAsyncThunk<getTaskResponse, { team_id: string}, { rejectValue: string }>('/getTask', async({team_id},{rejectWithValue}) => {
-  try {
-    const response = await axios.get<getTaskResponse>(
-      `https://morrow.hashim-dev007.online/task/getTasks/${team_id}`
+    const response = await taskApi.post<updateStatusResponse>(
+      `/update-status/${team_id}`,
+      { name, id }
     );
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError<{ message: string }>;
+    return rejectWithValue(
+      axiosError.response?.data.message || "failed to update the name"
+    );
+  }
+});
+
+export const deleteColumn = createAsyncThunk<
+  deleteStatusResponse,
+  { id: string; team_id: string },
+  { rejectValue: string }
+>("/deleteColumn", async ({ id, team_id }, { rejectWithValue }) => {
+  try {
+    const response = await taskApi.delete<deleteStatusResponse>(
+      `/delete-task/${team_id}?id=${id}`
+    );
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError<{ message: string }>;
+    return rejectWithValue(
+      axiosError.response?.data.message || "failed to delete the column"
+    );
+  }
+});
+
+export const createTask = createAsyncThunk<
+  ITask,
+  {
+    name: string;
+    id: string;
+    status: string;
+    priority: string;
+    team_id: string;
+    assignee: { _id: Types.ObjectId }[];
+  },
+  { rejectValue: string }
+>(
+  "/addTask",
+  async (
+    { name, id, priority, status, team_id, assignee },
+    { rejectWithValue }
+  ) => {
+    try {
+      const response = await taskApi.post<ITask>(
+        `/create-task/${team_id}?status=${status}&name=${name}&id=${id}&priority=${priority}`,
+        { assignee }
+      );
+      return response.data;
+    } catch (error) {
+      const axiosError = error as AxiosError<{ message: string }>;
+      return rejectWithValue(
+        axiosError.response?.data.message || "failed to create the Task"
+      );
+    }
+  }
+);
+
+export const getTasks = createAsyncThunk<
+  getTaskResponse,
+  { team_id: string },
+  { rejectValue: string }
+>("/getTask", async ({ team_id }, { rejectWithValue }) => {
+  try {
+    const response = await taskApi.get<getTaskResponse>(`/getTasks/${team_id}`);
     return response.data;
   } catch (error) {
     const axiosError = error as AxiosError<{ message: string }>;
@@ -102,42 +144,42 @@ export const getTasks = createAsyncThunk<getTaskResponse, { team_id: string}, { 
       axiosError.response?.data.message || "failed to get task"
     );
   }
-})
+});
 
-export const updateTaskStatus = createAsyncThunk<ITask , { team_id: string, id: string, status: string }, { rejectValue: string }>('/update-taskStatus', async ({ team_id, id, status },{rejectWithValue}) => {
-  try {
-    const response = await axios.put<ITask>(
-      `https://morrow.hashim-dev007.online/task/update-taskStatus/${team_id}?id=${id}&status=${status}`
-    );
-    return response.data;
-  } catch (error) {
-    const axiosError = error as AxiosError<{ message: string }>;
-    return rejectWithValue(
-      axiosError.response?.data.message || "failed to get task"
-    );
+export const updateTaskStatus = createAsyncThunk<
+  ITask,
+  { team_id: string; id: string; status: string },
+  { rejectValue: string }
+>(
+  "/update-taskStatus",
+  async ({ team_id, id, status }, { rejectWithValue }) => {
+    try {
+      const response = await taskApi.put<ITask>(
+        `/update-taskStatus/${team_id}?id=${id}&status=${status}`
+      );
+      return response.data;
+    } catch (error) {
+      const axiosError = error as AxiosError<{ message: string }>;
+      return rejectWithValue(
+        axiosError.response?.data.message || "failed to get task"
+      );
+    }
   }
-})
+);
 
 // Slice
 const BoardSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {
-    // clearError(state) {
-    //   state.error = null;
-    // },
-    // setError(state){
-    //   console.log("error")
-    //   state.error = state.error != null ? null:"error";
-    // }
-    setTask(state){
+    setTask(state) {
       state.tasks = state.tasks;
-    }
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(createColumn.fulfilled, (state, action) => {
       state.columns = [...state.columns, action.payload.data];
-      console.log("state,colu,m",state.columns)
+      console.log("state,column", state.columns);
     });
     builder.addCase(getColumn.fulfilled, (state, action) => {
       const data = action.payload.data;
@@ -166,7 +208,7 @@ const BoardSlice = createSlice({
       state.columns = [...filteredColumns];
     });
     builder.addCase(createTask.fulfilled, (state, action) => {
-      console.log("createTask", action.payload)
+      console.log("createTask", action.payload);
       state.tasks = [...state.tasks, action.payload];
       console.log(state.tasks);
     });
@@ -180,7 +222,7 @@ const BoardSlice = createSlice({
       );
     });
     builder.addCase(updateTaskStatus.fulfilled, (state, action) => {
-      state.tasks = state.tasks.map((task: ITask) => { 
+      state.tasks = state.tasks.map((task: ITask) => {
         if (task._id == action.payload._id) return action.payload;
         return task;
       });
