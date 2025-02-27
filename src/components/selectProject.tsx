3dd374830585a5b911ebe899049bcae5d9657f38
hashim <hashimkakkaden@gmail.com>
2025-02-27 10:10:32 +0530

    updatesonselectproject

--- src/components/selectProject.tsx
+++ src/components/selectProject.tsx
@@ -2,7 +2,7 @@ function SelectProject() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
         <p className="text-lg font-semibold text-red-600">Please select a project before accessing this page.</p>
-       </div>
+       </div>   
   )
 }
 
