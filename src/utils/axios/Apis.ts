412a6990e090fff92bb3a878724182644ad9bc6f
hashim <hashimkakkaden@gmail.com>
2025-03-21 12:49:11 +0530

    feat: added env files

--- src/utils/axios/Apis.ts
+++ src/utils/axios/Apis.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://morrow-backend.hashim-dev007.online",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
